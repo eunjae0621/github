16678df7c77c271b2be1acbd819372d8773051b9
eunjae0621 <chaej@sookmyung.ac.kr>
2025-08-23 15:16:03 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -3,30 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-	
-	int year, is_leap;
-	
-	printf("Input the year : ");
-	scanf("%d", &year);
-	
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))	
-	is_leap = 1;
-	else
-	is_leap = 0;
-	
-	printf("Is the year %d the leap year? %d\n", year, is_leap);
-	
-	int e,g; 
-	printf("Input two integers : ");
-	scanf("%i %i", &e, &g);
-	
-	printf("result is %d\n", e & g);
-	printf("result is %d\n", e | g);
-	printf("result is %d\n", e ^ g);
-	printf("result is %d\n", e << g);
-	printf("result is %d\n", e >> g);
-	
 int main(int argc, char *argv[]) {
-	printf("Hello, Sookmyung Electronics!\n"); //Hello, Sookmyung Electronics! ���
+	int input_int;
+	float input_float;
+	
+	printf("enter an integer : ");
+	scanf("%d", &input_int);
+	
+	printf("enter a float : ");
+	scanf("%f", &input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float);
+	
 	return 0;
 }
