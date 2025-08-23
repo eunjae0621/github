abc935790038b797c4a47df2578c1ba3f453216e
eunjae0621 <chaej@sookmyung.ac.kr>
2025-08-23 14:48:55 +0900

    week02

--- main.c
+++ main.c
@@ -3,38 +3,6 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	int x, y, z, m;
-	int a, b, c;
-	x = 2;
-	z = 1;
-	a = 3;
-	b = 4;
-	c = 5;
-	y = a * x * x + b * x + c;
-	m = (x + y + z) / 3;
-	
-	printf("y=%d, m=%d", y, m); 
-	
-	int d, f;
-	printf("input two integers : ");
-	scanf("%i %i", &d, &f);
-	
-	printf("%d + %d = %d\n", d, f, d + f);
-	printf("%d - %d = %d\n", d, f, d - f);
-	printf("%d * %d = %d\n", d, f, d * f);
-	printf("%d / %d = %d\n", d, f, d / f);
-	printf("%d %% %d = %d\n", d, f, d % f);
-	
-	int total_sec, min, sec;
-	
-	printf("Input the second : ");
-	scanf("%d", &total_sec);
-	
-	min = total_sec / 60;
-	sec = total_sec % 60;
-	
-	printf("The time is %d : %d\n", min, sec);
 	
 	int year, is_leap;
 	
@@ -58,5 +26,7 @@ int main(void) {
 	printf("result is %d\n", e << g);
 	printf("result is %d\n", e >> g);
 	
+int main(int argc, char *argv[]) {
+	printf("Hello, Sookmyung Electronics!\n"); //Hello, Sookmyung Electronics! ���
 	return 0;
 }
